dcc8f6ec57cb0ff473c7094eee7eaf9410ab9b15
Ross Cohen <rosscohen1990@gmail.com>
2023-05-13 04:11:07 -0400

    Update persona.ts

--- composables/persona.ts
+++ composables/persona.ts
@@ -11,7 +11,7 @@ export function usePersona() {
         if (!personaList.value.length) {
             await createPersona({
                 id: nanoid(),
-                title: 'Golem',
+                title: 'AI Tutor',
                 instructions: 'You are Golem, a large language model based assistant. Answer as concisely as possible.',
             })
         }
